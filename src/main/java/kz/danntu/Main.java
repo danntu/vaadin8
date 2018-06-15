48886a5237033717862d9498ca225a7b53200aa1
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-06-15 14:13:56 +0600

    Add ComboBox

--- src/main/java/kz/danntu/Main.java
+++ src/main/java/kz/danntu/Main.java
@@ -14,12 +14,29 @@ import javax.servlet.annotation.WebServlet;
 public class Main extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        VerticalLayout layout = new VerticalLayout();
-        Label label = new Label("Text");
-        Link linklabel = new Link("Label", new ExternalResource("/label/"));
-        layout.addComponent(label);
-        layout.addComponent(linklabel);
-        setContent(layout);
+        HorizontalLayout mainLayout = new HorizontalLayout();
+        setContent(mainLayout);
+
+        VerticalLayout primary = new VerticalLayout();
+        primary.setSizeFull();
+        Label labelString = new Label("Example for Link class");
+        primary.addComponent(labelString);
+        Label labelString2 = new Label("Example for MultiSelect");
+        primary.addComponent(labelString2);
+
+        VerticalLayout secondary = new VerticalLayout();
+        Link linkLabel = new Link("Open link http://host:port/label/", new ExternalResource("/label/"));
+        secondary.addComponent(linkLabel);
+        Link linkLabel2 = new Link("Open link http://host:port/combobox/", new ExternalResource("/combobox/"));
+        secondary.addComponent(linkLabel2);
+
+
+
+        mainLayout.addComponents(primary,secondary);
+        mainLayout.setComponentAlignment(primary,Alignment.TOP_CENTER);
+        mainLayout.setComponentAlignment(secondary,Alignment.TOP_CENTER);
+
+
     }
 
     @WebServlet(urlPatterns = "/*", name = "Main", asyncSupported = true)
--- src/main/java/kz/danntu/MyCombox.java
+++ src/main/java/kz/danntu/MyCombox.java
@@ -1,7 +1,16 @@
 package kz.danntu;
 
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.UI;
+
+import javax.servlet.annotation.WebServlet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mdaniyar on 6/15/18.
@@ -9,6 +18,20 @@ import com.vaadin.ui.UI;
 public class MyCombox extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
+        HorizontalLayout layout = new HorizontalLayout();
+        setContent(layout);
+
+        ComboBox comboBox = new ComboBox();
+        List<String> list = new ArrayList<>();
+        list.add("Test1");
+        list.add("Test2");
+        comboBox.setItems(list);
+        layout.addComponent(comboBox);
+        layout.setComponentAlignment(comboBox, Alignment.TOP_CENTER);
+    }
+    @WebServlet(urlPatterns = {"/combobox/*","/VAADIN/*"}, name = "MyUIServletLabel", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyCombox.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
 
     }
 }

4da223feae5367509f8120ad849d3879fa2188f3
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-06-15 12:05:33 +0600

    Label

---
+++ src/main/java/kz/danntu/MyCombox.java
@@ -0,0 +1,14 @@
+package kz.danntu;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+/**
+ * Created by mdaniyar on 6/15/18.
+ */
+public class MyCombox extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+}
--- src/main/java/kz/danntu/MyUILabel.java
+++ src/main/java/kz/danntu/MyUILabel.java
@@ -9,6 +9,8 @@ import com.vaadin.server.VaadinServlet;
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.*;
 
+import java.util.Arrays;
+
 /**
  * This UI is the application entry point. A UI may either represent a browser window 
  * (or tab) or some part of an HTML page where a Vaadin application is embedded.
@@ -22,60 +24,69 @@ public class MyUILabel extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        final Label label1 = new Label();
-        label1.setCaption("Empty constructor");
-        label1.setValue("Test1");
+        final HorizontalLayout twoColumns = new HorizontalLayout();
+        twoColumns.setWidth("100%");
+        twoColumns.setMargin(true);
+        twoColumns.setSpacing(true);
+        setContent(twoColumns);
 
-        final Label label2 = new Label("Test2");
-        label2.setCaption("With Parameter");
+        // first column
+        VerticalLayout primary = new VerticalLayout();
 
-        final Label label3 = new Label("Test3", ContentMode.HTML);
-        label3.setCaption("With Parameter and ContentMode");
-
-        TextField editor = new TextField("Input text",label3.getValue());
-        editor.addValueChangeListener(valueChangeEvent -> {
-           label3.setValue(valueChangeEvent.getValue());
-        });
-
-        Panel panel = new Panel("Panel Containing a Label and TextField");
-        panel.setWidth("600px");
-
-        FormLayout content = new FormLayout();
-        content.addComponents(label3,editor);
-        panel.setContent(content);
+        TextField firstName = buildTF("First name");
+        TextField lastName = buildTF("Last name");
+        TextField email = buildTF("email");
+        //PopupDateField birthDate = buildPDF("Date of Birth");
+        ComboBox gender = buildCB("Gender", "Male", "Female");
+        gender.setWidth("100%");
 
 
-        Label textLabel = new Label(
-                "Text where formatting characters, such as \\n, " +
-                        "and HTML, such as <b>here</b>, are quoted.",
-                ContentMode.TEXT);
+        primary.setCaption("Primary");
+        primary.addComponents(firstName, lastName, email, gender);
 
-        Label preLabel = new Label(
-                "Preformatted text is shown in an HTML <pre> tag.\n" +
-                        "Formatting such as\n" +
-                        " * newlines\n" +
-                        " * whitespace\n" +
-                        "and such are preserved. HTML tags, \n"+
-                        "such as <b>bold</b>, are quoted.",
-                ContentMode.PREFORMATTED);
+        twoColumns.addComponent(primary);
 
-        Label htmlLabel = new Label(
-                "In HTML mode, all HTML formatting tags, such as \n" +
-                        "<ul>"+
-                        " <li><b>bold</b></li>"+
-                        " <li>itemized lists</li>"+
-                        " <li>etc.</li>"+
-                        "</ul> "+
-                        "are preserved.",
-                ContentMode.HTML);
+        // second column
+        VerticalLayout secondary = new VerticalLayout();
+        secondary.setCaption("Secondary");
 
+        TextField weight = buildTF("Weight");
+        TextField height = buildTF("Height");
+        CheckBox active = new CheckBox("Active");
+        Label spacer = new Label("");
+        spacer.setHeight("62px");
+        HorizontalLayout checkbox = new HorizontalLayout();
+        checkbox.addComponents(active, spacer);
 
+        //PopupDateField registration = buildPDF("Time of registration");
+        ComboBox nationality = buildCB("Nationality", "German", "American",
+                "Finnish", "Polish");
 
-        layout.addComponents(label1,label2,panel,textLabel,preLabel,htmlLabel);
+        secondary.addComponents(weight, height, checkbox,
+                nationality);
 
-        setContent(layout);
+//        setup the checkbox
+//        active.setHeight("62px");
+        checkbox.setComponentAlignment(active, Alignment.MIDDLE_LEFT);
 
+        twoColumns.addComponent(secondary);
+    }
+    private ComboBox buildCB(String caption, String... options) {
+        ComboBox cb = new ComboBox(caption, Arrays.asList(options));
+        cb.setWidth("100%");
+        return cb;
+    }
+
+//    private PopupDateField buildPDF(String caption) {
+//        PopupDateField pdf = new PopupDateField(caption);
+//        pdf.setWidth("100%");
+//        return pdf;
+//    }
+
+    private TextField buildTF(String caption) {
+        TextField tf = new TextField(caption);
+        tf.setWidth("100%");
+        return tf;
     }
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServletLabel", asyncSupported = true)
